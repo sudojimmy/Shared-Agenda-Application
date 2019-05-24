8c106f1731c6e2270974f9f7f48829dbe78860a1
Alice Li <zhuoxili@hotmail.com>
2019-05-24 10:25:17 -0400

    fixed starterId check

--- Backend/service/src/main/java/controller/CreateEventController.java
+++ Backend/service/src/main/java/controller/CreateEventController.java
@@ -2,19 +2,16 @@ package controller;
 
 import constant.ApiConstant;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import store.DataStore;
-import types.Event;
 import types.CreateEventRequest;
 import types.CreateEventResponse;
-
-
-// for Alice only
-// ../gradlew clean build
+import types.Event;
 
 
 @RestController
@@ -29,25 +26,41 @@ public class CreateEventController extends BaseController {
             logger.error("Invalid Eventname!");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        if (request.getEventId() == null || request.getEventId().isEmpty()) {
-            logger.error("Invalid EventId!");
+        if (request.getStarterId() == null || request.getStarterId().isEmpty()) {
+            logger.error("Invalid StarterId!");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (request.getType() == null || request.getType().isEmpty()) {
+            logger.error("Invalid Type!");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
         // Step II: check restriction (conflict, or naming rules etc.)
         Document document = new Document();
-        document.put(ApiConstant.Event_Event_ID, request.getEventId());
-        if (dataStore.existInCollection(document, DataStore.COLLECTION_EVENTS)) {
-            logger.error("EventId Already Existed!");
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        document.put(ApiConstant.ACCOUNT_ACCOUNT_ID, request.getStarterId());
+        if (!dataStore.existInCollection(document, DataStore.COLLECTION_ACCOUNTS)) {
+            logger.error("StarterId is not Existed!");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         // Step III: write to Database
-        Event p = new Event().withEventname(request.getEventname()).withEventId(request.getEventId());
+        ObjectId groupId = new ObjectId();
+        String id = groupId.toString();
+        Event p = new Event().
+                withEventname(request.getEventname()).
+                withStarterId(request.getStarterId()).
+                withType(request.getType()).
+                withStart(request.getStart()).
+                withCount(request.getCount()).
+                withDate(request.getDate()).
+                withLocation(request.getLocation()).
+                withRepeat(request.getRepeat()).
+                withState(request.getState()).
+                withDescription(request.getDescription());
         dataStore.insertToCollection(p, DataStore.COLLECTION_EVENTS);
 
         // Step IV: create response object
-        return new ResponseEntity<>(new CreateEventResponse().withEventId(request.getEventId()),
-                HttpStatus.OK);
+        return new ResponseEntity<>(new CreateEventResponse().withEventId(id),
+                HttpStatus.CREATED);
     }
 }
