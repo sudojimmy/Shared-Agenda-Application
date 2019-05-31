d013f78f5a5aece3fc02109fc33b2affcd37b9e3
ylqm <ylqmriki@gmail.com>
2019-05-31 16:44:03 -0400

    draft

--- Backend/service/src/main/java/constant/ApiConstant.java
+++ Backend/service/src/main/java/constant/ApiConstant.java
@@ -7,7 +7,11 @@ public class ApiConstant {
     public static final String ACCOUNT_ACCOUNT_DESCRIPTION = "description";
 
     // Event
-    public static final String Event_Event_ID = "eventId";
-    public static final String Event_Event_NAME = "eventname";
+    public static final String EVENT_EVENT_ID = "eventId";
+    public static final String EVENT_EVENT_NAME = "eventname";
     public static final String Event_Event_DESCRIPTION = "description";
+
+    // Group
+    public static final String GROUP_ID = "_id";
+    public static final String GROUP_MEMBERS = "members";
 }
