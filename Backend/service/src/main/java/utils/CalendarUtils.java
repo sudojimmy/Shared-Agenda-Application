8cb1d02bc526d016016d4541778a4f31a71fcf96
j585zhan <j585zhan@uwaterloo.ca>
2019-06-10 14:40:56 -0400

    added util files to clean up the redundant code

---
+++ Backend/service/src/main/java/utils/CalendarUtils.java
@@ -0,0 +1,27 @@
+package utils;
+
+import constant.ApiConstant;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import store.DataStore;
+import types.Calendar;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static controller.BaseController.dataStore;
+
+public class CalendarUtils {
+    public static Calendar createCalendarToDatabase() {
+        String calendarId = new ObjectId().toString();
+        Calendar calendar = new Calendar().withCalendarId(calendarId).withEventList(new ArrayList<>());
+        dataStore.insertToCollection(calendar, DataStore.COLLECTION_CALENDARS);
+        return calendar;
+    }
+
+    public static Calendar getCalendar(String calendarId) {
+        Document document = new Document();
+        document.put(ApiConstant.CALENDAR_CALENDAR_ID, calendarId);
+        return dataStore.findOneInCollection(document, DataStore.COLLECTION_CALENDARS);
+    }
+}
