302a4937e47caf72d6997d92259bf49150ae04d7
Zhuoxi Li <z565li@uwaterloo.ca>
2019-06-14 14:42:10 -0400

    #41: InviteEvent Controller

--- Backend/service/src/main/java/utils/EventMessageUtils.java
+++ Backend/service/src/main/java/utils/EventMessageUtils.java
@@ -1,21 +1,16 @@
 package utils;
 
-import constant.ApiConstant;
-import org.bson.Document;
 import org.bson.types.ObjectId;
 import store.DataStore;
-import types.Event;
-import types.EventMessage;
-
-import java.util.ArrayList;
+import types.*;
 
 import static controller.BaseController.dataStore;
 
 public class EventMessageUtils {
     public static EventMessage createEventMessageToDatabase(String eventId, final String eventname, final String starterId,
-                                                            final Event.Type type, final int start, final int count,
-                                                            final String date, final String location, final Event.Repeat repeat,
-                                                            final Event.State state, final String description, boolean isPublic) {
+                                                            final EventType type, final int start, final int count,
+                                                            final String date, final String location, final EventRepeat repeat,
+                                                            final EventState state, final String description, boolean isPublic) {
         if (eventId == null) {
             eventId = new ObjectId().toString();
         }
