c9c3d3de4c00720e268f61ff8b43b913146db356
ylqm <ylqmriki@gmail.com>
2019-06-14 23:28:22 -0400

    create get group controller and add group utility functions

---
+++ Backend/service/src/main/java/controller/GetGroupController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import constant.ApiConstant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import types.Group;
+import types.GetGroupRequest;
+import types.GetGroupResponse;
+import utils.AccountUtils;
+import utils.GroupUtils;
+
+@RestController
+public class GetGroupController extends BaseController {
+
+    @PostMapping("/getGroup")
+    public ResponseEntity<GetGroupResponse> handle(@RequestBody GetGroupResponse request) {
+        logger.info("getGroup: " + request);
+
+        assertPropertyValid(request.getGroupId(), ApiConstant.GROUP_ID);
+
+        GroupUtils.checkGroupExist(request.getGroupId());
+
+        Group group = GroupUtils.getGroup(request.getGroupId());
+        assertDatabaseObjectFound(group, ApiConstant.GROUP_ID);
+
+        return new ResponseEntity<>(new GetGroupResponse()
+            .withGroupId(group.getGroupId())
+            .withName(group.getName())
+            .withDescription(group.getDescription())
+            .withMembers(group.getMembers())
+            .withOwnerId(group.getOwnerId()),
+            HttpStatus.OK);
+    }
+}
