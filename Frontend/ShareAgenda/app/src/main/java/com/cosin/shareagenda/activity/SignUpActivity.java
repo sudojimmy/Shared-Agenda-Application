e8613d2544d3ca9b6ae1ed443610aac63d255081
Jiaming Zhang <sudojimmy@gmail.com>
2019-06-23 00:30:17 -0400

    added signup and signin logic (#79)
    * added signup and signin logic
    
    * move endpoint to constant

--- Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/activity/SignUpActivity.java
+++ Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/activity/SignUpActivity.java
@@ -1,11 +1,21 @@
 package com.cosin.shareagenda.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cosin.shareagenda.R;
+import com.cosin.shareagenda.model.ApiClient;
+import com.cosin.shareagenda.model.ApiException;
+import com.cosin.shareagenda.model.Model;
+
+import types.Account;
+import types.CreateAccountResponse;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText etNickName;
@@ -20,5 +30,31 @@ public class SignUpActivity extends AppCompatActivity {
         etDescription = findViewById(R.id.etDescription);
         btnSignUpSubmit = findViewById(R.id.btnSignUp);
 
+        Activity activity = this;
+        btnSignUpSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivityForResult(signInIntent, 101); // TODO follow pattern
+                String name = etNickName.getText().toString();
+                String description = etDescription.getText().toString();
+                CreateAccountResponse resp = null;
+                try {
+                    resp = ApiClient.createAccount(name, description);
+                } catch (ApiException e) {
+                    Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Model.model.setUser(new Account()
+                        .withAccountId(resp.getAccountId())
+                        .withCalendarId(resp.getCalendarId())
+                        .withDescription(description)
+                        //.withMessageQueueId(resp.get)
+                        .withNickname(name));
+
+                Intent intent = new Intent(activity, ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/model/ApiClient.java
+++ Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/model/ApiClient.java
@@ -1,5 +1,6 @@
 package com.cosin.shareagenda.model;
 
+import com.google.api.client.http.HttpStatusCodes;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -10,22 +11,40 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import types.Account;
+import types.CreateAccountRequest;
+import types.CreateAccountResponse;
 import types.GetAccountRequest;
 
 import static com.cosin.shareagenda.config.SystemConfig.SHARED_AGENDA_API_URL;
+import static com.cosin.shareagenda.model.ApiEndpoint.CREATE_ACCOUNT;
+import static com.cosin.shareagenda.model.ApiEndpoint.GET_ACCOUNT;
 
 public class ApiClient {
     public static final MediaType MEDOA_JSON = MediaType.parse("application/json; charset=utf-8");
     static OkHttpClient client = new OkHttpClient();
     static Gson gson = new Gson();
 
-    public static Account getAccount(String accountId) {
+    public static Account getAccount(String accountId) throws ApiException {
         GetAccountRequest getAccountRequest = new GetAccountRequest().withAccountId(accountId);
-        String body = post("getAccount", gson.toJson(getAccountRequest));
+        String body = post(GET_ACCOUNT, gson.toJson(getAccountRequest));
         return gson.fromJson(body, Account.class);
     }
 
-    public static String post(String endpoint, String json) {
+    public static CreateAccountResponse createAccount(String nickname, String description) throws ApiException {
+        CreateAccountRequest createAccountRequest = new CreateAccountRequest()
+                .withAccountId(getAccountId())
+                .withNickname(nickname)
+                .withDescription(description);
+        String body = post(CREATE_ACCOUNT, gson.toJson(createAccountRequest));
+        System.out.println(body);
+        return gson.fromJson(body, CreateAccountResponse.class);
+    }
+
+    private static String getAccountId() {
+        return Model.model.getGoogleSignInAccount().getEmail();
+    }
+
+    public static String post(String endpoint, String json) throws ApiException {
         try {
             return makePostRequest(endpoint, json);
         } catch (IOException e) {
@@ -33,14 +52,22 @@ public class ApiClient {
         }
     }
 
-    private static String makePostRequest(String endpoint, String json) throws IOException {
+    private static String makePostRequest(String endpoint, String json) throws IOException, ApiException {
         RequestBody body = RequestBody.create(MEDOA_JSON, json);
         Request request = new Request.Builder()
                 .url(SHARED_AGENDA_API_URL + endpoint)
                 .post(body)
+                .addHeader("google-token", getUserToken())
                 .build();
         Response response = client.newCall(request).execute();
-
+        int rc = response.code();
+        if (!HttpStatusCodes.isSuccess(rc)) {
+            throw new ApiException(response.message(), rc);
+        }
         return response.body().string();
+    }
+
+    private static String getUserToken() {
+        return Model.model.getGoogleSignInAccount().getIdToken();
     }
 }
