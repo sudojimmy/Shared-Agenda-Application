0a9aaa5cb06298e99d9b28811d4cbea86cf1ead8
Zhuoxi Li <z565li@uwaterloo.ca>
2019-07-17 00:44:46 -0400

    removed unused code

--- Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/dialog/AddFriendDialog.java
+++ Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/dialog/AddFriendDialog.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.widget.Toast;
 
-import com.alamkanak.weekview.WeekView;
 import com.cosin.shareagenda.access.net.CallbackHandler;
 import com.cosin.shareagenda.adapter.SearchFriendsAdapter;
 import com.cosin.shareagenda.api.ApiClient;
--- Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/dialog/DisplayAddAccountDialog.java
+++ Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/dialog/DisplayAddAccountDialog.java
@@ -1,36 +1,17 @@
 package com.cosin.shareagenda.dialog;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.core.content.ContextCompat;
 
 import com.cosin.shareagenda.R;
 import com.cosin.shareagenda.access.net.CallbackHandler;
-import com.cosin.shareagenda.activity.ContactsActivity;
 import com.cosin.shareagenda.adapter.SearchFriendsAdapter;
 import com.cosin.shareagenda.api.ApiClient;
-import com.cosin.shareagenda.api.ApiErrorResponse;
-import com.cosin.shareagenda.entity.DisplayableEvent;
-import com.google.common.io.Resources;
-import com.google.gson.Gson;
-
-import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
-import types.Account;
-import types.CreateAccountResponse;
-import types.GetAccountResponse;
-import types.GetFriendQueueResponse;
-
-import static com.cosin.shareagenda.access.net.CallbackHandler.HTTP_FAILURE;
-import static com.cosin.shareagenda.access.net.CallbackHandler.SUCCESS;
 
 public class DisplayAddAccountDialog extends DisplayAccountBaseDialog {
     //   private Account account;
