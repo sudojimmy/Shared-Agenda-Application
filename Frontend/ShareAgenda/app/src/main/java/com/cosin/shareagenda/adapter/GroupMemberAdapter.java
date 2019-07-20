4ec05150a77f14e4c6afe204825e334e8855c8d2
Alice Li <z565li@uwaterloo.ca>
2019-07-19 22:56:01 -0400

    refine create group (#148)
    * refine create group
    
    * show GroupName on group calendar

--- Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/adapter/GroupMemberAdapter.java
+++ Frontend/ShareAgenda/app/src/main/java/com/cosin/shareagenda/adapter/GroupMemberAdapter.java
@@ -1,22 +1,30 @@
 package com.cosin.shareagenda.adapter;
 
-import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cosin.shareagenda.R;
 import com.cosin.shareagenda.entity.VO_Member;
 
 import java.util.List;
 
+import types.Account;
+
 public class GroupMemberAdapter extends RecyclerView.Adapter<GroupMemberAdapter.ViewHolder> {
     private List<VO_Member> members;
+    private Context context;
 
-    public GroupMemberAdapter(List<VO_Member> members) {
+    public GroupMemberAdapter(List<VO_Member> members, Context context) {
         this.members = members;
+        this.context = context;
     }
 
     public void setMembers(List<VO_Member> members) {
@@ -27,11 +35,13 @@ public class GroupMemberAdapter extends RecyclerView.Adapter<GroupMemberAdapter.
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
         LinearLayout linearLayout;
+        CheckBox checkBox;
 
         public ViewHolder(View view) {
             super(view);
             textView = view.findViewById(R.id.tvGroupContactName);
             linearLayout = view.findViewById(R.id.llCreateGroupItem);
+            checkBox = view.findViewById(R.id.checkBox);
         }
     }
 
@@ -46,7 +56,10 @@ public class GroupMemberAdapter extends RecyclerView.Adapter<GroupMemberAdapter.
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         VO_Member member = members.get(position);
-        viewHolder.textView.setText(member.getMember());
+        Account memberAccount = member.getMember();
+
+        String nicknameAddEmail = memberAccount.getNickname() + "(" + memberAccount.getAccountId() + ")";
+        viewHolder.textView.setText(nicknameAddEmail);
         viewHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,10 +67,37 @@ public class GroupMemberAdapter extends RecyclerView.Adapter<GroupMemberAdapter.
                 VO_Member member = members.get(position);
                 if (member.isElected()) {
                     member.setElected(false);
+                    viewHolder.checkBox.setChecked(false);
                     ((LinearLayout)v).setBackgroundColor(0x00000000);
                 } else {
+                    // selected
                     member.setElected(true);
-                    ((LinearLayout)v).setBackgroundColor(0xaa88ffaa);
+                    viewHolder.checkBox.setChecked(true);
+                    ((LinearLayout)v).setBackgroundColor(
+                            ContextCompat.getColor(context, R.color.light_blue));
+                }
+            }
+        });
+
+        viewHolder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View checkboxview) {
+                //int position = viewHolder.getAdapterPosition();
+                VO_Member member = members.get(position);
+                View v = viewHolder.linearLayout;
+
+                if (!viewHolder.checkBox.isChecked()) {
+                    member.setElected(false);
+                    viewHolder.checkBox.setChecked(false);
+
+                    ((LinearLayout)v).setBackgroundColor(0x00000000);
+                } else {
+                    // selected
+                    member.setElected(true);
+                    viewHolder.checkBox.setChecked(true);
+
+                    ((LinearLayout)v).setBackgroundColor(
+                            ContextCompat.getColor(context, R.color.light_blue));
                 }
             }
         });
